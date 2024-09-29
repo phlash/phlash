33ace037a6c4266f5f7288691af469bc0da44062
Phil Ashby <phil.sourcehut@ashbysoft.com>
2024-09-29 09:39:39 +0100

    updated by push.sh

--- push.sh
+++ push.sh
@@ -2,5 +2,6 @@
 # auto-push this repo from crontab
 cd `dirname $0`
 git add --all
-(git status |fgrep 'nothing to commit') && exit 0
-echo 'would commit'
+(git status |fgrep -q 'nothing to commit') && exit 0
+git commit -m 'updated by push.sh'
+git push
