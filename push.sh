766d5e3cb1e715693a30f745dd2b39de212ec56a
Phil Ashby <phil.sourcehut@ashbysoft.com>
2024-09-29 09:38:10 +0100

    typo

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@
 # auto-push this repo from crontab
 cd `dirname $0`
 git add --all
-(git status |fgrep 'nothing to commmit') && exit 0
+(git status |fgrep 'nothing to commit') && exit 0
 echo 'would commit'
